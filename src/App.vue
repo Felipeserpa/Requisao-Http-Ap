07797b2806054093f9844db8cfe13a099d51c832
Felipeserpa <felipe.mago_skt@hotmail.com>
2023-08-24 11:59:59 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+
+<header>
+  Vue nutri
+</header>
+
+<article v-for="item in nutri" :key="item.id">
+
+<strong>{{item.titulo}}</strong><br>
+
+<img src="item.capa"/><br>
+
+<span>{{item.categoria}}</span>
+
+<p>{{item.subtitulo}}</p>   
+</article>
+
+
+
+  </div>
+
+
+
+ 
+
+
+</template>
+
+<script>
+ import api from "./services/api";
+
+
+  export default{
+
+    name: 'App',
+    data(){
+      return{
+       nutri:[]
+
+      }
+    },
+    
+     async created(){
+
+const response =  await api.get('/?api=posts');
+console.log(response.data)
+this.nutri = response.data
+
+    }
+  
+
+    
+  
+  }
+  
+</script>
+
+<style>
+
+
+
+</style>
---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const api =axios.create({
+
+    baseURL:'https://sujeitoprogramador.com/rn-api/'
+})
+
+export default api;
